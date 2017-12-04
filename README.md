65757f065f9fae57b15197cda59d57ce7e2e0ad9
PavelShturm85 <pavel.shturm@gmail.com>
2017-12-04 16:57:54 +0400

    Скипт выводит имена отправляющих задания после полуночи и время.

--- README.md
+++ README.md
@@ -1,6 +1,37 @@
 # Night Owls Detector
 
-[TODO. There will be project description]
+**The module outputs  midnighters and their time to sent tasks**
+
+
+# Quickstart
+**Ways to use:**
+- Have to use  module `seek_dev_nighters.py` after `python3`.
+
+
+Example of script launch on Linux, Python 3.5:
+
+
+```bash
+$ seek_dev_nighters.py
+idnighter name: DenisBeskov, 	 time of sending: 04.12.17 03:27
+Midnighter name: shewolf664050, 	 time of sending: 04.12.17 02:53
+Midnighter name: id460393476, 	 time of sending: 04.12.17 02:06
+Midnighter name: id460393476, 	 time of sending: 04.12.17 02:00
+Midnighter name: id460393476, 	 time of sending: 04.12.17 01:54
+Midnighter name: SergeiKhrustalev, 	 time of sending: 04.12.17 00:50
+Midnighter name: id460393476, 	 time of sending: 04.12.17 00:36
+Midnighter name: id460393476, 	 time of sending: 04.12.17 00:18
+Midnighter name: id460393476, 	 time of sending: 04.12.17 00:13
+Midnighter name: SergeiKhrustalev, 	 time of sending: 04.12.17 00:05
+Midnighter name: MLMaterials, 	 time of sending: 03.12.17 03:45
+Midnighter name: MLMaterials, 	 time of sending: 03.12.17 03:05
+Midnighter name: MLMaterials, 	 time of sending: 03.12.17 02:58
+Midnighter name: abbakar, 	 time of sending: 03.12.17 02:06
+
+
+```
+
+
 
 # Project Goals
 
--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,15 +1,33 @@
-def load_attempts():
-    pages = 1
-    for page in range(pages):
-        # FIXME подключить загрузку данных из API
-        yield {
-            'username': 'bob',
-            'timestamp': 0,
-            'timezone': 'Europe/Moscow',
-        }
+import pytz
+import requests
+from datetime import datetime
 
-def get_midnighters():
-    pass
+
+def load_attempts():
+    for page in range(1, 11):
+        url = 'https://devman.org/api/challenges/solution_attempts/?page={}' \
+            .format(page)
+        solutions_attempts = requests.get(url).json()['records']
+        for attempt in solutions_attempts:
+            yield attempt
+
+
+def get_midnighters_and_time(attempts):
+    for attempt in attempts:
+        utc_timezone = pytz.timezone('UTC')
+        utc_datatime = utc_timezone.localize(
+            datetime.utcfromtimestamp(attempt['timestamp']))
+        timezone = pytz.timezone(attempt['timezone'])
+        data_time = timezone.normalize(utc_datatime.astimezone(timezone))
+        hours = datetime.strftime(data_time, "%H")
+        if 0 <= int(hours) <= 6:
+            yield attempt['username'], datetime.strftime(
+                data_time, "%d.%m.%y %H:%M")
+        else:
+            None
+
 
 if __name__ == '__main__':
-  pass
+    midnighters = get_midnighters_and_time(load_attempts())
+    for name, time in midnighters:
+        print('Midnighter name: {}, \t time of sending: {}'.format(name, time))
